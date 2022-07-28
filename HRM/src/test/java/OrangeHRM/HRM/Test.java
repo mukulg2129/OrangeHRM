2a30e1911faee0fcb12f7b62cb1ede58f5739a58
mukulg2129 <guptamukul2129@gmail.com>
2022-07-28 17:59:21 +0530

    intial push

--- HRM/src/test/java/OrangeHRM/HRM/Test.java
+++ HRM/src/test/java/OrangeHRM/HRM/Test.java
@@ -44,7 +44,7 @@ public class Test {
 		String jobTitleF= e.getText();
 		System.out.println(jobTitleF);
 
-		Assert.assertEquals(jobTitle, jobTitleF);
+	Assert.assertEquals(jobTitle, jobTitleF);
 	}
 	
 	
